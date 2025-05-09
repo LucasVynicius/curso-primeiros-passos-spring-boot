7fecce0d39a35a9e04e056623eedf438a7e50612
Lucas Vynicius Gonçalves Albuquerque <lucas.a@SUDEMAPB.INTRA>
2025-05-09 14:27:35 -0300

    feat: atualizando endpoint salvarProduto

--- produtosapi/src/main/java/br/com/cursoudemy/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/br/com/cursoudemy/produtosapi/controller/ProdutoController.java
@@ -1,4 +1,16 @@
 package br.com.cursoudemy.produtosapi.controller;
 
+import br.com.cursoudemy.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("produto")
 public class ProdutoController {
+
+    @PostMapping
+    public Produto salvarProduto(@RequestBody Produto produto){
+        System.out.println("Produto salvo: " + produto);
+
+        return produto;
+    }
 }
--- produtosapi/src/main/java/br/com/cursoudemy/produtosapi/model/Produto.java
+++ produtosapi/src/main/java/br/com/cursoudemy/produtosapi/model/Produto.java
@@ -1,4 +1,51 @@
 package br.com.cursoudemy.produtosapi.model;
 
 public class Produto {
+
+    private String id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
 }
