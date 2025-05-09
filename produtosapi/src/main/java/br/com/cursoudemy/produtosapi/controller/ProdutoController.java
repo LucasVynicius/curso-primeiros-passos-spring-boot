2f1ef5730bfe56849b10af0d3bd729d3fd2fbfd9
Lucas Vynicius Gonçalves Albuquerque <lucas.a@SUDEMAPB.INTRA>
2025-05-09 15:36:09 -0300

    Feat: Persistindo produtos no BD

--- produtosapi/src/main/java/br/com/cursoudemy/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/br/com/cursoudemy/produtosapi/controller/ProdutoController.java
@@ -1,16 +1,28 @@
 package br.com.cursoudemy.produtosapi.controller;
 
 import br.com.cursoudemy.produtosapi.model.Produto;
+import br.com.cursoudemy.produtosapi.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+
     @PostMapping
     public Produto salvarProduto(@RequestBody Produto produto){
         System.out.println("Produto salvo: " + produto);
 
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        
+        produtoRepository.save(produto);
         return produto;
     }
 }
